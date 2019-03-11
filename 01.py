78ca3b4d7a2a6c956782c133cdd26ed97fd12bf5
mihael.ruzic@gmail.com <mihael.ruzic@hnb.hr>
2019-03-11 15:21:52 +0100

    proba 3

--- 01.py
+++ 01.py
@@ -1,1 +1,24 @@
-print('Hello world')
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': 'Openshift Jenkins Pipeline Python/Nginx Implementation',
+        'description': 'Find the implementation at https://github.com/ruddra/openshift-python-nginx'
+    },
+    {
+        'id': 2,
+        'title': 'Openshift Jenkins Pipeline Django Implementation',
+        'description': 'Find the implementation at https://github.com/ruddra/openshift-django'
+    }
+]
+
+@app.route('/', methods=['GET'])
+def get_tasks():
+    #return jsonify({'tasks': tasks})
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host='localhost')
